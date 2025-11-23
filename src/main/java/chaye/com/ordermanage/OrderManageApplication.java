4aab4757516ee05e9270b9755116283267f6968a
chaye <852635477@qq.com>
2025-11-23 14:00:24 +0800

    init

---
+++ src/main/java/chaye/com/ordermanage/OrderManageApplication.java
@@ -0,0 +1,13 @@
+package chaye.com.ordermanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManageApplication.class, args);
+    }
+
+}
